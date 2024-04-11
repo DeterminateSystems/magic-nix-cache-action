a87d983438863e64834e11e9fa8b0648f836f94d
Graham Christensen <graham@grahamc.com>
2024-04-11 19:21:15 -0400

    Rebase on detsys-ts and get rid of bun

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,69 @@
-// Main
+import * as fs from "node:fs/promises";
+import * as os from "node:os";
+import * as path from "node:path";
+import { spawn, exec, SpawnOptions } from "node:child_process";
+import { openSync, readFileSync } from "node:fs";
+import { inspect, promisify } from "node:util";
+import * as http from "http";
 
-import * as fs from 'node:fs/promises';
-import * as os from 'node:os';
-import * as path from 'node:path';
-import { spawn, exec, SpawnOptions } from 'node:child_process';
-import { openSync, readFileSync } from 'node:fs';
-import { inspect, promisify } from 'node:util';
-import * as http from 'http';
-
-import * as core from '@actions/core';
-import { Tail } from 'tail';
+import * as core from "@actions/core";
+import { Tail } from "tail";
 import got from "got";
+import { IdsToolbox } from "detsys-ts";
 
-const ENV_CACHE_DAEMONDIR = 'MAGIC_NIX_CACHE_DAEMONDIR';
+const ENV_CACHE_DAEMONDIR = "MAGIC_NIX_CACHE_DAEMONDIR";
 
 const gotClient = got.extend({
   retry: {
     limit: 1,
-    methods: [ 'POST', 'GET', 'PUT', 'HEAD', 'DELETE', 'OPTIONS', 'TRACE' ],
+    methods: ["POST", "GET", "PUT", "HEAD", "DELETE", "OPTIONS", "TRACE"],
   },
   hooks: {
     beforeRetry: [
       (error, retryCount) => {
         core.info(`Retrying after error ${error.code}, retry #: ${retryCount}`);
-      }
+      },
     ],
   },
 });
 
-
-function getCacherUrl() : string {
-  const runnerArch = process.env.RUNNER_ARCH;
-  const runnerOs = process.env.RUNNER_OS;
-  const binarySuffix = `${runnerArch}-${runnerOs}`;
-  const urlPrefix = `https://install.determinate.systems/magic-nix-cache-closure`;
-  if (core.getInput('source-url')) {
-    return core.getInput('source-url');
-  }
-
-  if (core.getInput('source-tag')) {
-    return `${urlPrefix}/tag/${core.getInput('source-tag')}/${binarySuffix}`;
-  }
-
-  if (core.getInput('source-pr')) {
-    return `${urlPrefix}/pr/${core.getInput('source-pr')}/${binarySuffix}`;
-  }
-
-  if (core.getInput('source-branch')) {
-    return `${urlPrefix}/branch/${core.getInput('source-branch')}/${binarySuffix}`;
-  }
-
-  if (core.getInput('source-revision')) {
-    return `${urlPrefix}/rev/${core.getInput('source-revision')}/${binarySuffix}`;
-  }
-
-  return `${urlPrefix}/stable/${binarySuffix}`;
-}
-
-async function fetchAutoCacher() {
-  const binary_url = getCacherUrl();
-  core.info(`Fetching the Magic Nix Cache from ${binary_url}`);
-
-  const { stdout } = await promisify(exec)(`curl -L "${binary_url}" | xz -d | nix-store --import`);
+async function fetchAutoCacher(toolbox: IdsToolbox): Promise<string> {
+  const closurePath = await toolbox.fetch();
+  toolbox.recordEvent("load_closure");
+  const { stdout } = await promisify(exec)(
+    `cat "${closurePath}" | xz -d | nix-store --import`,
+  );
 
   const paths = stdout.split(os.EOL);
-
   // Since the export is in reverse topologically sorted order, magic-nix-cache is always the penultimate entry in the list (the empty string left by split being the last).
   const last_path = paths.at(-2);
-
   return `${last_path}/bin/magic-nix-cache`;
 }
 
-function tailLog(daemonDir) {
-  const log = new Tail(path.join(daemonDir, 'daemon.log'));
+function tailLog(daemonDir: string): Tail {
+  const log = new Tail(path.join(daemonDir, "daemon.log"));
   core.debug(`tailing daemon.log...`);
-  log.on('line', (line) => {
+  log.on("line", (line) => {
     core.info(line);
   });
   return log;
 }
 
-async function setUpAutoCache() {
-  const tmpdir = process.env['RUNNER_TEMP'] || os.tmpdir();
-  const required_env = ['ACTIONS_CACHE_URL', 'ACTIONS_RUNTIME_URL', 'ACTIONS_RUNTIME_TOKEN'];
+async function setUpAutoCache(toolbox: IdsToolbox): Promise<void> {
+  const tmpdir = process.env["RUNNER_TEMP"] || os.tmpdir();
+  const required_env = [
+    "ACTIONS_CACHE_URL",
+    "ACTIONS_RUNTIME_URL",
+    "ACTIONS_RUNTIME_TOKEN",
+  ];
 
-  var anyMissing = false;
+  let anyMissing = false;
   for (const n of required_env) {
     if (!process.env.hasOwnProperty(n)) {
       anyMissing = true;
-      core.warning(`Disabling automatic caching since required environment ${n} isn't available`);
+      core.warning(
+        `Disabling automatic caching since required environment ${n} isn't available`,
+      );
     }
   }
 
@@ -96,37 +71,37 @@ async function setUpAutoCache() {
     return;
   }
 
-  core.debug(`GitHub Action Cache URL: ${process.env['ACTIONS_CACHE_URL']}`);
+  core.debug(`GitHub Action Cache URL: ${process.env["ACTIONS_CACHE_URL"]}`);
 
-  const daemonDir = await fs.mkdtemp(path.join(tmpdir, 'magic-nix-cache-'));
+  const daemonDir = await fs.mkdtemp(path.join(tmpdir, "magic-nix-cache-"));
 
-  var daemonBin: string;
-  if (core.getInput('source-binary')) {
-    daemonBin = core.getInput('source-binary');
+  let daemonBin: string;
+  if (core.getInput("source-binary")) {
+    daemonBin = core.getInput("source-binary");
   } else {
-    daemonBin = await fetchAutoCacher();
+    daemonBin = await fetchAutoCacher(toolbox);
   }
 
-  var runEnv;
+  let runEnv;
   if (core.isDebug()) {
     runEnv = {
       RUST_LOG: "trace,magic_nix_cache=debug,gha_cache=debug",
       RUST_BACKTRACE: "full",
-      ...process.env
+      ...process.env,
     };
   } else {
     runEnv = process.env;
   }
 
-  const notifyPort = core.getInput('startup-notification-port');
+  const notifyPort = core.getInput("startup-notification-port");
 
   const notifyPromise = new Promise<Promise<void>>((resolveListening) => {
     const promise = new Promise<void>(async (resolveQuit) => {
       const notifyServer = http.createServer((req, res) => {
-        if (req.method === 'POST' && req.url === '/') {
+        if (req.method === "POST" && req.url === "/") {
           core.debug(`Notify server shutting down.`);
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.end('{}');
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end("{}");
           notifyServer.close(() => {
             resolveQuit();
           });
@@ -142,34 +117,45 @@ async function setUpAutoCache() {
 
   // Start tailing the daemon log.
   const outputPath = `${daemonDir}/daemon.log`;
-  const output = openSync(outputPath, 'a');
+  const output = openSync(outputPath, "a");
   const log = tailLog(daemonDir);
   const netrc = await netrcPath();
   const nixConfPath = `${process.env["HOME"]}/.config/nix/nix.conf`;
 
   const daemonCliFlags: string[] = [
-    '--startup-notification-url', `http://127.0.0.1:${notifyPort}`,
-    '--listen', core.getInput('listen'),
-    '--upstream', core.getInput('upstream-cache'),
-    '--diagnostic-endpoint', core.getInput('diagnostic-endpoint'),
-    '--nix-conf', nixConfPath
-  ].concat(
-    core.getBooleanInput('use-flakehub') ? [
-      '--use-flakehub',
-      '--flakehub-cache-server', core.getInput('flakehub-cache-server'),
-      '--flakehub-api-server', core.getInput('flakehub-api-server'),
-      '--flakehub-api-server-netrc', netrc,
-      '--flakehub-flake-name', core.getInput('flakehub-flake-name'),
-    ] : []).concat(
-      core.getBooleanInput('use-gha-cache') ? [
-        '--use-gha-cache'
-      ] : []);
+    "--startup-notification-url",
+    `http://127.0.0.1:${notifyPort}`,
+    "--listen",
+    core.getInput("listen"),
+    "--upstream",
+    core.getInput("upstream-cache"),
+    "--diagnostic-endpoint",
+    core.getInput("diagnostic-endpoint"),
+    "--nix-conf",
+    nixConfPath,
+  ]
+    .concat(
+      core.getBooleanInput("use-flakehub")
+        ? [
+            "--use-flakehub",
+            "--flakehub-cache-server",
+            core.getInput("flakehub-cache-server"),
+            "--flakehub-api-server",
+            core.getInput("flakehub-api-server"),
+            "--flakehub-api-server-netrc",
+            netrc,
+            "--flakehub-flake-name",
+            core.getInput("flakehub-flake-name"),
+          ]
+        : [],
+    )
+    .concat(core.getBooleanInput("use-gha-cache") ? ["--use-gha-cache"] : []);
 
   const opts: SpawnOptions = {
-    stdio: ['ignore', output, output],
+    stdio: ["ignore", output, output],
     env: runEnv,
-    detached: true
-  }; 
+    detached: true,
+  };
 
   // Display the final command for debugging purposes
   core.debug("Full daemon start command:");
@@ -178,16 +164,22 @@ async function setUpAutoCache() {
   // Start the server. Once it is ready, it will notify us via the notification server.
   const daemon = spawn(daemonBin, daemonCliFlags, opts);
 
-  const pidFile = path.join(daemonDir, 'daemon.pid');
+  const pidFile = path.join(daemonDir, "daemon.pid");
   await fs.writeFile(pidFile, `${daemon.pid}`);
 
   core.info("Waiting for magic-nix-cache to start...");
 
   await new Promise<void>((resolve, reject) => {
-    notifyPromise.then((value) => {
-      resolve();
-    });
-    daemon.on('exit', async (code, signal) => {
+    notifyPromise
+      // eslint-disable-next-line github/no-then
+      .then((_value) => {
+        resolve();
+      })
+      // eslint-disable-next-line github/no-then
+      .catch((err) => {
+        reject(new Error(`error in notifyPromise: ${err}`));
+      });
+    daemon.on("exit", async (code, signal) => {
       if (signal) {
         reject(new Error(`Daemon was killed by signal ${signal}`));
       } else if (code) {
@@ -200,13 +192,13 @@ async function setUpAutoCache() {
 
   daemon.unref();
 
-  core.info('Launched Magic Nix Cache');
+  core.info("Launched Magic Nix Cache");
   core.exportVariable(ENV_CACHE_DAEMONDIR, daemonDir);
 
   log.unwatch();
 }
 
-async function notifyAutoCache() {
+async function notifyAutoCache(): Promise<void> {
   const daemonDir = process.env[ENV_CACHE_DAEMONDIR];
 
   if (!daemonDir) {
@@ -215,9 +207,10 @@ async function notifyAutoCache() {
 
   try {
     core.debug(`Indicating workflow start`);
-    const res: any = await gotClient.post(`http://${core.getInput('listen')}/api/workflow-start`).json();
-    core.debug(`back from post`);
-    core.debug(res);
+    const res: Response = await gotClient
+      .post(`http://${core.getInput("listen")}/api/workflow-start`)
+      .json();
+    core.debug(`back from post: ${res}`);
   } catch (e) {
     core.info(`Error marking the workflow as started:`);
     core.info(inspect(e));
@@ -225,26 +218,31 @@ async function notifyAutoCache() {
   }
 }
 
-
-async function netrcPath() {
-  const expectedNetrcPath = path.join(process.env['RUNNER_TEMP'], 'determinate-nix-installer-netrc')
+async function netrcPath(): Promise<string> {
+  const expectedNetrcPath = path.join(
+    process.env["RUNNER_TEMP"] || os.tmpdir(),
+    "determinate-nix-installer-netrc",
+  );
   try {
-    await fs.access(expectedNetrcPath)
+    await fs.access(expectedNetrcPath);
     return expectedNetrcPath;
   } catch {
     // `nix-installer` was not used, the user may be registered with FlakeHub though.
-    const destinedNetrcPath = path.join(process.env['RUNNER_TEMP'], 'magic-nix-cache-netrc')
+    const destinedNetrcPath = path.join(
+      process.env["RUNNER_TEMP"] || os.tmpdir(),
+      "magic-nix-cache-netrc",
+    );
     try {
       await flakehub_login(destinedNetrcPath);
     } catch (e) {
       core.info("FlakeHub cache disabled.");
-      core.debug(`Error while logging into FlakeHub: ${e}`)
+      core.debug(`Error while logging into FlakeHub: ${e}`);
     }
     return destinedNetrcPath;
   }
 }
 
-async function flakehub_login(netrc: string) {
+async function flakehub_login(netrc: string): Promise<void> {
   const jwt = await core.getIDToken("api.flakehub.com");
 
   await fs.writeFile(
@@ -259,16 +257,16 @@ async function flakehub_login(netrc: string) {
   core.info("Logged in to FlakeHub.");
 }
 
-async function tearDownAutoCache() {
+async function tearDownAutoCache(): Promise<void> {
   const daemonDir = process.env[ENV_CACHE_DAEMONDIR];
 
   if (!daemonDir) {
-    core.debug('magic-nix-cache not started - Skipping');
+    core.debug("magic-nix-cache not started - Skipping");
     return;
   }
 
-  const pidFile = path.join(daemonDir, 'daemon.pid');
-  const pid = parseInt(await fs.readFile(pidFile, { encoding: 'ascii' }));
+  const pidFile = path.join(daemonDir, "daemon.pid");
+  const pid = parseInt(await fs.readFile(pidFile, { encoding: "ascii" }));
   core.debug(`found daemon pid: ${pid}`);
   if (!pid) {
     throw new Error("magic-nix-cache did not start successfully");
@@ -278,9 +276,10 @@ async function tearDownAutoCache() {
 
   try {
     core.debug(`about to post to localhost`);
-    const res: any = await gotClient.post(`http://${core.getInput('listen')}/api/workflow-finish`).json();
-    core.debug(`back from post`);
-    core.debug(res);
+    const res: Response = await gotClient
+      .post(`http://${core.getInput("listen")}/api/workflow-finish`)
+      .json();
+    core.debug(`back from post: ${res}`);
   } finally {
     core.debug(`unwatching the daemon log`);
     log.unwatch();
@@ -288,40 +287,31 @@ async function tearDownAutoCache() {
 
   core.debug(`killing`);
   try {
-    process.kill(pid, 'SIGTERM');
+    process.kill(pid, "SIGTERM");
   } catch (e) {
-    if (e.code !== 'ESRCH') {
+    if (typeof e === "object" && e && "code" in e && e.code !== "ESRCH") {
       throw e;
     }
   } finally {
     if (core.isDebug()) {
       core.info("Entire log:");
-      const log = readFileSync(path.join(daemonDir, 'daemon.log'));
-      core.info(log.toString());
+      const entireLog = readFileSync(path.join(daemonDir, "daemon.log"));
+      core.info(entireLog.toString());
     }
   }
 }
 
-const isPost = !!process.env['STATE_isPost'];
+const idslib = new IdsToolbox({
+  name: "magic-nix-cache",
+  fetchStyle: "gh-env-style",
+});
 
-try {
-  if (!isPost) {
-    core.saveState('isPost', 'true');
-    await setUpAutoCache();
-    await notifyAutoCache();
-  } else {
-    await tearDownAutoCache();
-  }
-} catch (e) {
-  core.info(`got an exception:`);
-  core.info(e);
+idslib.onMain(async () => {
+  await setUpAutoCache(idslib);
+  await notifyAutoCache();
+});
+idslib.onPost(async () => {
+  await tearDownAutoCache();
+});
 
-  if (!isPost) {
-    core.setFailed(e.message);
-    throw e;
-  } else {
-    core.info("not considering this a failure: finishing the upload is optional, anyway.");
-    process.exit();
-  }}
-
-core.debug(`rip`);
+idslib.execute();
