9d46dc3d80edb2c8494f1b9ce62084126982807d
Graham Christensen <graham@grahamc.com>
2025-06-18 15:31:33 -0400

    Way more builds lol

--- .github/workflows/cache-tester.nix
+++ .github/workflows/cache-tester.nix
@@ -7,4 +7,16 @@ derivation {
   args = [ "-euxc" "echo \"$seed\" > $out" ];
 
   inherit seed;
+
+  deps = builtins.map
+    (n: derivation {
+      name = "cache-test-${toString n}";
+      system = builtins.currentSystem;
+
+      builder = "/bin/sh";
+      args = [ "-euxc" "echo \"$seed\" > $out" ];
+
+      inherit seed;
+    })
+    (builtins.genList (n: n) 1000);
 }
